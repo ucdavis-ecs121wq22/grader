2a5cbfa047588ebfa498b1c0c0e296f69518223c
Karan Mehta <mehtakaran9@gmail.com>
2022-01-20 12:57:21 -0800

    Adding tests (#1)

--- hw1/tests/test_branch_sort_strings_exist.py
+++ hw1/tests/test_branch_sort_strings_exist.py
@@ -1,13 +1,11 @@
 import subprocess
 
-def test_does_branches_exist_as_specified():
+def test_does_sort_strings_branch_exist():
     branch_cmd = """
     git branch -r
     """
     add_sort_strings_branch_exists = False
-    add_cli_branch_exists = False
-    main_branch_exists = False
-
+    
     log_branch = subprocess.check_output(branch_cmd, shell=True, text=True, stderr=subprocess.STDOUT)
     
     if log_branch != None:
@@ -15,11 +13,5 @@ def test_does_branches_exist_as_specified():
             branch = branch.replace('origin/', '').strip()
             if (branch == "add/sort_strings"):
                 add_sort_strings_branch_exists = True
-            elif (branch == "add/cli"):
-                add_cli_branch_exists = True
-            elif (branch == "main"):
-                main_branch_exists = True
 
     assert add_sort_strings_branch_exists == True
-    assert add_cli_branch_exists == True
-    assert main_branch_exists == True
---
+++ hw1/tests/test_main_branch_exist.py
@@ -0,0 +1,17 @@
+import subprocess
+
+def test_does_cli_branch_exist():
+    branch_cmd = """
+    git branch -r
+    """
+    main_branch_exists = False
+    
+    log_branch = subprocess.check_output(branch_cmd, shell=True, text=True, stderr=subprocess.STDOUT)
+    
+    if log_branch != None:
+        for branch in log_branch.split("\n"):
+            branch = branch.replace('origin/', '').strip()
+            if (branch == "main"):
+                main_branch_exists = True
+
+    assert main_branch_exists == True
