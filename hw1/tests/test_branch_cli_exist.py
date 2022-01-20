23098edc5c4894f2098bddf424e66f6ef08a9bc2
mehtakaran9 <mehtakaran9@gmail.com>
2022-01-20 12:29:07 -0800

    Fixing

--- hw1/tests/test_branch_cli_exist.py
+++ hw1/tests/test_branch_cli_exist.py
@@ -12,6 +12,6 @@ def test_does_cli_branch_exist():
         for branch in log_branch.split("\n"):
             branch = branch.replace('origin/', '').strip()
             if (branch == "add/cli"):
-                add_sort_strings_branch_exists = True
+                add_cli_branch_exists = True
 
     assert add_cli_branch_exists == True
